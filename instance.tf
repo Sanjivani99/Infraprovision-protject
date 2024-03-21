212a52f3db5b806587696a7c7d578b8c7e100214
Sanjivani99 <123535837+Sanjivani99@users.noreply.github.com>
2024-03-21 13:01:20 +0530

    Delete instance.tf

--- instance.tf
+++
@@ -1,9 +0,0 @@
-resource "aws_instance" "main" {
-  ami           = var.ami 
-  instance_type = var.ec2type
-  subnet_id     = aws_subnet.main.id
-  #count = 2
-  tags = {
-    Name = "myserver"
-  }
-}
